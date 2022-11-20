dd8f6651983feba5b0228c1ad29d054280ea452c
Osman0202 <Osmanpanpanza@yandex.ru>
2022-11-20 23:32:07 +0600

    turtle

---
+++ turtle.py
@@ -0,0 +1,19 @@
+import turtle
+a = 0
+b = 0
+
+turtle.bgcolor("green")
+turtle.speed(0)
+turtle.pencolor("black")
+turtle.penup()
+turtle.goto(0, 200)
+turtle.pendown()
+
+while True:
+    turtle.forward(a)
+    turtle.rigth(b)
+    a += 3
+    b += 1
+    if b == 200:
+        break
+turtle.exitonclick()
